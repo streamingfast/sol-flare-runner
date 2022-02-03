10cea76406162a3230f16114ee165a00faa0ee0e
Julien Cassis <juliencassis@gmail.com>
2022-02-03 12:03:19 -0500

    Added more to install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,4 +2,9 @@
 
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 mkdir -p ~/app
-cp config.yam ~/app/config.yaml
+cp config.yaml ~/app/config.yaml
+cp download.sh ~/app/download.sh
+cp identity.json ~/app/identity.json
+cp start.sh ~/app/start.sh
+cp monitor.sh ~/app/monitor.sh
+
